2cd1ab11bf29106377dcc009562e4026f782d4b9
Sanggyu Nam <pokeplus@gmail.com>
2017-01-10 15:55:05 +0900

    Add a user interface for deploying a mashup

--- powmash-uploader.py
+++ powmash-uploader.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/deploy', methods=['GET',])
+def deploy():
+    return render_template('deploy.html')
 
 
 if __name__ == '__main__':
